f7c4eb4db4f0698bb6afec35c86caa4975cadfa5
E.J. Schroeder <eschroeder@callibrity.com>
2017-12-05 12:45:18 -0500

    Change indentation and update some logging

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 const qs = require('querystring');
-const debug = require('debug')('dorbit-slackbot:index');
+const debugRouter = require('debug')('dorbit-slackbot:router');
 const debugSlackApi = require('debug')('dorbit-slackbot:slack-api');
 const debugSlackHook = require('debug')('dorbit-slackbot:slack-hook');
 
 const app = express();
 const slack = axios.create({
-	baseURL: process.env.SLACK_API_BASE_URL,
-	timeout: 5000,
-	headers: {
-		'Content-Type': 'application/json',
+  baseURL: process.env.SLACK_API_BASE_URL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
     'Authorization': `Bearer ${process.env.SLACK_ACCESS_TOKEN}`
-	}
+  }
 });
 const slackDoorbellMessages = JSON.parse(process.env.SLACK_DOORBELL_MESSAGES);
 
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  debug("Received a request: " + req.method + " " + req.url);
+  debugRouter("Received a request: " + req.method + " " + req.url);
   res.send('<h3>Dorbit is running! :D</h3>');
 });
 
@@ -31,7 +31,7 @@ app.post('/ring', (req, res) => {
   const { source_id, token } = req.body;
 
   if (token === process.env.SOURCE_TOKEN) {
-    debug("Event from source: " + source_id);
+    debugRouter("Event from source: " + source_id);
     res.sendStatus(200);
     let messageIndex = Math.floor(Math.random() * slackDoorbellMessages.length);
 
@@ -64,7 +64,7 @@ app.post('/ring', (req, res) => {
       }
     }).catch(slackWebhookErrorHandle);
   } else {
-    debug("Token received in ring payload was invalid.");
+    debugRouter("Token received in ring payload was invalid.");
     res.sendStatus(403);
   }
 });
@@ -100,7 +100,6 @@ function slackWebhookErrorHandle(error) {
   } else {
 	  ebugSlackHook('Error ' + error.message);
   }
-  console.log(error.config);
 }
 
 app.listen(process.env.SLACKBOT_PORT, () => {
