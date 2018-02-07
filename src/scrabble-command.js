aeafce5e68ee82815180f553caeb06b4babe6d00
E.J. Schroeder <eschroeder@callibrity.com>
2018-02-07 14:26:49 -0500

    Add scrabblize functionality

--- src/scrabble-command.js
+++ src/scrabble-command.js
@@ -1,8 +1,50 @@
 const debugSlackHook = require('debug')('dorbit-slackbot:scrabble-command');
+const Entities = require('html-entities').AllHtmlEntities;
+const entities = new Entities();
+const VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
 
 module.exports = (req, res) => {
-  const body = JSON.parse(req.body);
+  const { token, text } = JSON.parse(req.body);
 
-  debugSlackHook(body);
-  res.sendStatus(200);
+  if (token !== VERIFICATION_TOKEN) {
+    debugSlackHook("Token received does not match token from Slack");
+    debugSlackHook(req.body);
+    res.sendStatus(403);
+    return;
+  }
+
+  scrabblizedString = scrabblize(text);
+
+  res.send({
+    response_type: "in_channel",
+    text: scrabblizedString
+  });
+}
+
+function scrabblize(text) {
+  var tokenizedText = text.split(" ");
+
+  var convertedWords = tokenizedText.map(function(word) {
+    if (isSlackEntity(word)) {
+      return word;
+    }
+    return replaceLettersWithScrabbleTiles(word);
+  });
+
+  return convertedWords.join(":blank_scrabble:");
+}
+
+function isSlackEntity(word) {
+  return word.startsWith('<') && word.endsWith('>');
+}
+
+function replaceLettersWithScrabbleTiles(word) {
+  var rawText = entities.decode(word);
+  var lowerText = rawText.toLowerCase();
+
+  var result = lowerText.replace(/[a-z]/g, function(x) {
+    return ":" + x + "_scrabble:";
+  });
+
+  return entities.encode(result);
 }
--- src/slack-webhooks.js
+++ src/slack-webhooks.js
@@ -1,9 +1,10 @@
 const debugSlackHook = require('debug')('dorbit-slackbot:slack-hook');
+const VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
 
 module.exports = (req, res) => {
   const { callback_id, user, token } = JSON.parse(req.body.payload);
 
-  if (token !== process.env.SLACK_VERIFICATION_TOKEN) {
+  if (token !== VERIFICATION_TOKEN) {
     debugSlackHook("Token received does not match token from Slack");
     debugSlackHook(req.body);
     res.sendStatus(403);
