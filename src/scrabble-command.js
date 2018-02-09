9a3d1a6b5ab45b9d70a4d2cde96b5828ff7a6910
E.J. Schroeder <eschroeder@callibrity.com>
2018-02-09 14:48:30 -0500

    Update to better handle Slack entities and ignore emojis in certain cases

--- src/scrabble-command.js
+++ src/scrabble-command.js
@@ -1,6 +1,7 @@
 const debugSlackHook = require('debug')('dorbit-slackbot:scrabble-command');
 const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
+const S = require('string');
 const VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
 
 module.exports = (req, res) => {
@@ -13,6 +14,7 @@ module.exports = (req, res) => {
     return;
   }
 
+  debugSlackHook('Text: ' + text);
   scrabblizedString = scrabblize(text);
 
   res.send({
@@ -35,7 +37,11 @@ function scrabblize(text) {
 }
 
 function isSlackEntity(word) {
-  return word.startsWith('<') && word.endsWith('>');
+  return (word.includes('<') && word.includes('>')) || (word.startsWith(":") && word.endsWith(":"));
+}
+
+function isLetter(char) {
+  return char.length === 1 && char.match(/[a-z]/i);
 }
 
 function replaceLettersWithScrabbleTiles(word) {
